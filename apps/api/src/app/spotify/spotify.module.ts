a906d16fb2723b18eea0b68f83248e7f1950cc0b
Staijn1 <stein@jnkr.eu>
2022-07-24 14:21:24 +0200

    Init file service

--- apps/api/src/app/spotify/spotify.module.ts
+++ apps/api/src/app/spotify/spotify.module.ts
@@ -1,10 +1,11 @@
-import {Global, Module} from '@nestjs/common';
+import {Module} from '@nestjs/common';
 import {SpotifyService} from './spotify.service';
 import {HttpModule} from '@nestjs/axios'
+import {UtilModule} from '../modules/util/util.module';
 
 @Module({
   providers: [SpotifyService],
-  imports: [HttpModule],
+  imports: [HttpModule, UtilModule],
   exports: [SpotifyService],
 })
 export class SpotifyModule {
