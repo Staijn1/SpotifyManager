4b7c08c196e383c7f196ea6bbe7cdf5c30b41b26
Staijn1 <stein@jnkr.eu>
2022-07-26 20:30:00 +0200

    Update playlist.module.ts

--- apps/api/src/app/modules/playlist/playlist.module.ts
+++ apps/api/src/app/modules/playlist/playlist.module.ts
@@ -3,11 +3,12 @@ import {PlaylistController} from './controllers/playlist-controller/playlist.con
 import {PlaylistService} from './services/playlist/playlist.service';
 import {SpotifyModule} from '../../spotify/spotify.module';
 import {UtilModule} from '../util/util.module';
+import {PlaylistFileService} from './services/playlist-file-service/playlist-file.service';
 
 @Module({
   imports: [SpotifyModule, UtilModule],
   controllers: [PlaylistController],
-  providers: [PlaylistService]
+  providers: [PlaylistService, PlaylistFileService]
 })
 export class PlaylistModule {
 }
