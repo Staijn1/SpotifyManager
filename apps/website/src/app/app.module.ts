9ae099b263c1af4ec2c6269d1f766bc0e64b8350
Staijn1 <stein@jnkr.eu>
2022-08-22 15:58:34 +0200

    Removed old component which will not be needed for new design

--- apps/website/src/app/app.module.ts
+++ apps/website/src/app/app.module.ts
@@ -1,25 +1,24 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AccountComponent } from './pages/account/account.component';
-import { AuthorizeComponent } from './pages/authorize/authorize.component';
-import { LoginComponent } from './pages/login/login.component';
-import { OverviewComponent } from './pages/overview/overview.component';
-import { ForkSyncComponent } from './pages/fork-sync/fork-sync.component';
-import { ErrorComponent } from './components/error/error.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NavbarComponent } from './main/navbar/navbar.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { AvatarComponent } from './components/avatar/avatar.component';
-import { BoxComponent } from './components/box/box.component';
-import { InnerPageComponent } from './main/inner-page/inner-page.component';
-import { ForkComponent } from './pages/fork/fork.component';
-import { PlaylistOverviewComponent } from './components/playlist-overview/playlist-overview.component';
-import { PlaylistComparePageComponent } from './pages/playlist-compare-page/playlist-compare-page.component';
-import { ModalComponent } from './components/modal/modal.component';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AccountComponent} from './pages/account/account.component';
+import {AuthorizeComponent} from './pages/authorize/authorize.component';
+import {LoginComponent} from './pages/login/login.component';
+import {OverviewComponent} from './pages/overview/overview.component';
+import {ForkSyncComponent} from './pages/fork-sync/fork-sync.component';
+import {ErrorComponent} from './components/error/error.component';
+import {HomeComponent} from './pages/home/home.component';
+import {NavbarComponent} from './main/navbar/navbar.component';
+import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
+import {AvatarComponent} from './components/avatar/avatar.component';
+import {BoxComponent} from './components/box/box.component';
+import {ForkComponent} from './pages/fork/fork.component';
+import {PlaylistOverviewComponent} from './components/playlist-overview/playlist-overview.component';
+import {PlaylistComparePageComponent} from './pages/playlist-compare-page/playlist-compare-page.component';
+import {ModalComponent} from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +27,6 @@ import { ModalComponent } from './components/modal/modal.component';
     AuthorizeComponent,
     NavbarComponent,
     HomeComponent,
-    InnerPageComponent,
     OverviewComponent,
     AccountComponent,
     ForkSyncComponent,
@@ -45,4 +43,5 @@ import { ModalComponent } from './components/modal/modal.component';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
