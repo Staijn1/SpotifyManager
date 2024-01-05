1fd2bff2ee154e09f333943313c28790c7ba0c2c
Staijn1 <stein@jnkr.eu>
2024-01-05 16:01:17 +0100

    Update audio.component.ts

--- apps/website/src/app/components/audio/audio.component.ts
+++ apps/website/src/app/components/audio/audio.component.ts
@@ -10,8 +10,16 @@ export class AudioComponent {
   @Input() src: string | undefined;
   @Input() type = 'audio/mpeg';
 
+  /**
+   * Constructor
+   * @param audioService
+   */
   constructor(private audioService: AudioService) {}
 
+  /**
+   * Handle play event
+   * @param event
+   */
   handlePlay(event: Event) {
     const audioElement = event.target as HTMLAudioElement;
     this.audioService.playNextAudioAndPauseCurrentlyPlaying(audioElement).then();
