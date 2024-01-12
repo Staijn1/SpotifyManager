81a77fe905e370e212f60dea07df4b512a7cb26c
Stein Jonker <stein@jnkr.eu>
2024-01-12 13:56:45 +0100

    Create spotify artist and track component

---
+++ apps/website/src/app/components/spotify-artist/spotify-artist.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-spotify-artist',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './spotify-artist.component.html',
+  styleUrl: './spotify-artist.component.scss'
+})
+export class SpotifyArtistComponent {
+  @Input() artist: SpotifyApi.ArtistObjectFull | undefined;
+}
---
+++ apps/website/src/app/components/spotify-track/spotify-track.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spotify-manager-spotify-track',
+  standalone: true,
+  imports: [],
+  templateUrl: './spotify-track.component.html',
+  styleUrl: './spotify-track.component.css'
+})
+export class SpotifyTrackComponent {
+
+}
