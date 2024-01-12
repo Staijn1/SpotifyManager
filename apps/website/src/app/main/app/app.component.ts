5f37b4d35b90558502a201927da9998660c9cb45
Stein Jonker <stein@jnkr.eu>
2024-01-12 12:22:22 +0100

    Show messages from the message service

--- apps/website/src/app/main/app/app.component.ts
+++ apps/website/src/app/main/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
+import { MessageService } from '../../services/message/message.service';
+import { ToastComponent } from '../../components/toast/toast.component';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, NavigationBarComponent],
+  imports: [RouterOutlet, NavigationBarComponent, ToastComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(protected readonly messageService: MessageService) {
+  }
 }
