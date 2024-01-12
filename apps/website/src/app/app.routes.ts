f2dd1952f1cc5fc9dad2e74a4914dd119424bf41
Stein Jonker <stein@jnkr.eu>
2024-01-12 10:22:25 +0100

    Create a mermaid component to render mermaid diagrams

--- apps/website/src/app/app.routes.ts
+++ apps/website/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Route } from '@angular/router';
-import { GetStartedComponent } from './pages/get-started/get-started/get-started.component';
+import { GetStartedComponent } from './pages/get-started/get-started.component';
 import { AuthorizeComponent } from './pages/authorize/authorize.component';
 import { AccountPageComponent } from './pages/account/account-page.component';
 
---
+++ apps/website/src/app/pages/get-started/get-started.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { SpotifyAuthenticationService } from '../../services/spotify-authentication/spotify-authentication.service';
+import { TabsComponent } from '../../components/tab-component/tabs.component';
+import { MermaidComponent } from '../../components/mermaid-diagram/mermaid.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './get-started.component.html',
+  imports: [
+    TabsComponent,
+    MermaidComponent
+  ],
+  styleUrls: ['./get-started.component.scss']
+})
+export class GetStartedComponent implements OnInit {
+  readonly playlistEvolutionGraph = `
+  %%{
+    init: {
+        'theme': 'base',
+        'gitGraph': {
+            'showBranches': true,
+            'showCommitLabel':true,
+            'mainBranchName': 'Original Playlist'
+        }
+    }
+}%%
+gitGraph
+    commit id: "Song A added"
+    commit id: "Song B deleted"
+
+    branch "Remixed Playlist"
+    commit id: "Add Personal Song"
+
+    checkout "Original Playlist"
+    commit id: "Song C Added"
+
+    checkout "Remixed Playlist"
+    commit id: "Remove song A from Original playlist"
+
+
+    checkout "Original Playlist"
+    commit id: "Song D Added"
+
+    checkout "Remixed Playlist"
+    merge "Original Playlist" tag:"Sync Result"
+
+    commit id: "..."
+
+    checkout "Original Playlist"
+    commit id: ".."
+  `;
+  readonly spotify = faSpotify;
+  isLoading = false;
+
+  /**
+   * Inject spotify Auth service since we are logging in
+   * @param spotifyAuth
+   */
+  constructor(readonly spotifyAuth: SpotifyAuthenticationService) {
+  }
+
+  /**
+   * Initialize this component by generating the URL where the user can log into spotify
+   */
+  ngOnInit(): void {
+    this.isLoading = true;
+  }
+
+  /**
+   * Redirect to the login page of Spotify, to the URL created during initialization
+   */
+  login(): void {
+    this.spotifyAuth.initializeAuthorizitionFlow();
+  }
+}
