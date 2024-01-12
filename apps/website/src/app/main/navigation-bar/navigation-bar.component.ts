6da992df05a5f06b3b592973a1988a1da843a9a3
Stein Jonker <stein@jnkr.eu>
2024-01-12 16:01:33 +0100

    Add account to navbar

--- apps/website/src/app/main/navigation-bar/navigation-bar.component.ts
+++ apps/website/src/app/main/navigation-bar/navigation-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SpotifyAuthenticationService } from '../../services/spotify-authentication/spotify-authentication.service';
 
 @Component({
   standalone: true,
@@ -11,4 +12,5 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navigation-bar.component.scss',
 })
 export class NavigationBarComponent {
+  constructor(protected readonly spotifyAuthService: SpotifyAuthenticationService) {}
 }
