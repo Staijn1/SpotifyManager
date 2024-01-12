d3081d52c67f9af538bf43e1c78b386793d1d748
Stein Jonker <stein@jnkr.eu>
2024-01-12 17:30:24 +0100

    Fix tests

--- apps/website/src/app/main/app/app.component.spec.ts
+++ apps/website/src/app/main/app/app.component.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, RouterTestingModule],
+      providers: [{
+        provide: OAuthService, useValue: {
+          configure: jest.fn()
+        }
+      }]
     }).compileComponents();
   });
 
@@ -13,5 +19,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  })
+  });
 });
