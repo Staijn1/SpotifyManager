27015c9abe77fee7dd764122cf44a64a54a503f8
Stein Jonker <stein@jnkr.eu>
2024-01-12 15:04:48 +0100

    Update spotify artist component

--- apps/website/src/app/components/spotify-artist/spotify-artist.component.ts
+++ apps/website/src/app/components/spotify-artist/spotify-artist.component.ts
@@ -1,14 +1,36 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-spotify-artist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './spotify-artist.component.html',
   styleUrl: './spotify-artist.component.scss'
 })
 export class SpotifyArtistComponent {
   @Input() artist: SpotifyApi.ArtistObjectFull | undefined;
   @Input() ranking: number | undefined;
+
+  get displayName(): string {
+    const artistName = this.artist?.name ?? '';
+    const prefix = this.ranking ? `${this.ranking}. ` : '';
+    return `${prefix}${artistName}`;
+  };
+
+  /**
+   * Returns the genres of the artist, by capitalizing the first letter of each genre and joining them with a |
+   */
+  get genres(): string {
+    const genres = this.artist?.genres.map(genre => genre.charAt(0).toUpperCase() + genre.slice(1));
+    return genres?.join(' | ') ?? '';
+  }
+
+  get popularity(): number {
+    return this.artist?.popularity ?? 0;
+  }
+
+  get image(): SpotifyApi.ImageObject | undefined {
+    return this.artist?.images[1];
+  }
 }
