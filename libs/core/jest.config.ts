4749e49f1e18b487c3237d6c9bcbe0692cea4379
Stein Jonker <stein@jnkr.eu>
2024-01-12 11:57:04 +0100

    Update jest configs

--- libs/core/jest.config.ts
+++ libs/core/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/core',
+  coverageDirectory: '../../coverage/libs/core',
 };
