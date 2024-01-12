319664c5f8fec30827253d8713408687f1d119d6
Stein Jonker <stein@jnkr.eu>
2024-01-12 14:57:24 +0100

    Add ranking to artists

--- apps/website/src/app/components/spotify-artist/spotify-artist.component.ts
+++ apps/website/src/app/components/spotify-artist/spotify-artist.component.ts
@@ -10,4 +10,5 @@ import { CommonModule } from '@angular/common';
 })
 export class SpotifyArtistComponent {
   @Input() artist: SpotifyApi.ArtistObjectFull | undefined;
+  @Input() ranking: number | undefined;
 }
