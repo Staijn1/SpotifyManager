3d3b3846676df3ea496cd2d05352161df41b6d97
Staijn1 <stein@jnkr.eu>
2024-01-14 16:09:54 +0100

    Update sync-remixed-playlist-page.component.spec.ts

--- apps/website/src/app/pages/sync-remixed-playlist/sync-remixed-playlist-page.component.spec.ts
+++ apps/website/src/app/pages/sync-remixed-playlist/sync-remixed-playlist-page.component.spec.ts
@@ -1,13 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SyncRemixedPlaylistPageComponent } from './sync-remixed-playlist-page.component';
+import { ApiService } from '../../services/api/api.service';
+import { Router } from '@angular/router';
 
-describe('PlaylistComparePageComponent', () => {
+describe('SyncRemixedPlaylistPageComponent', () => {
   let component: SyncRemixedPlaylistPageComponent;
   let fixture: ComponentFixture<SyncRemixedPlaylistPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SyncRemixedPlaylistPageComponent],
+      providers: [
+        Router,
+        {
+          provide: ApiService,
+          useValue: {
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SyncRemixedPlaylistPageComponent);
