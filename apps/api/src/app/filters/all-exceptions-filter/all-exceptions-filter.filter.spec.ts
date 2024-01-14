af6614208009e843b4a026abdaecbe2fc4956aeb
Staijn1 <stein@jnkr.eu>
2024-01-14 12:59:16 +0100

    Fix test for AllExceptionsFilter

--- apps/api/src/app/filters/all-exceptions-filter/all-exceptions-filter.filter.spec.ts
+++ apps/api/src/app/filters/all-exceptions-filter/all-exceptions-filter.filter.spec.ts
@@ -1,7 +1,28 @@
 import { AllExceptionsFilter } from './all-exceptions-filter.filter';
+import { HttpAdapterHost, AbstractHttpAdapter } from '@nestjs/core';
+import { Test } from '@nestjs/testing';
 
 describe('AllExceptionsFilterFilter', () => {
+  let mockHttpAdapterHost: HttpAdapterHost;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        {
+          provide: HttpAdapterHost,
+          useValue: {
+            httpAdapter: {
+              // Mock the methods you need
+            },
+          },
+        },
+      ],
+    }).compile();
+
+    mockHttpAdapterHost = moduleRef.get<HttpAdapterHost>(HttpAdapterHost);
+  });
+
   it('should be defined', () => {
-    expect(new AllExceptionsFilter()).toBeDefined();
+    expect(new AllExceptionsFilter(mockHttpAdapterHost)).toBeDefined();
   });
 });
