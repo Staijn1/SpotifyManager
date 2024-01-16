f6a666d3a4c68b4426fef96bde325a24f5561689
Staijn1 <stein@jnkr.eu>
2024-01-16 23:34:29 +0100

    Add a preview to track component

--- apps/website/src/app/components/spotify-track/spotify-track.component.ts
+++ apps/website/src/app/components/spotify-track/spotify-track.component.ts
@@ -1,12 +1,15 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ArtistObjectSimplified, EpisodeObjectFull, PlaylistTrackObject, TrackObjectFull } from '@spotify-manager/core';
 import { NgClass } from '@angular/common';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-spotify-track',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    FaIconComponent
   ],
   templateUrl: './spotify-track.component.html',
   styleUrl: './spotify-track.component.scss',
@@ -29,6 +32,18 @@ export class SpotifyTrackComponent {
   @Input() ranking: number | undefined;
   @Input() imageClasses = 'w-full';
   @Input() reverse = false;
+  @Input() showPreview = false;
+  @ViewChild('audioElement') audioElement!: ElementRef;
+  @ViewChild('progressBar') progressBar!: ElementRef;
+  previewPlayIcon = faPlay;
+
+  get previewUrl(): string | undefined {
+    if (this.track?.type == 'track') {
+      return this.track.preview_url;
+    }
+
+    return undefined;
+  };
 
   get coverImage(): string {
     if (this.track?.type == 'track') {
@@ -45,4 +60,22 @@ export class SpotifyTrackComponent {
 
     return 'Episodes not supported yet';
   };
+
+
+  updatePreviewProgressBar() {
+    const audio = this.audioElement.nativeElement;
+    const progress = this.progressBar.nativeElement;
+    progress.value = (audio.currentTime / audio.duration) * 100;
+  }
+
+  togglePreview() {
+    const audio = this.audioElement.nativeElement;
+    if (audio.paused) {
+      audio.play();
+      this.previewPlayIcon = faPause;
+    } else {
+      audio.pause();
+      this.previewPlayIcon = faPlay;
+    }
+  }
 }
