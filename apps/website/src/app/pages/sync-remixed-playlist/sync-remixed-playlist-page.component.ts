3a11f320109777ae216501ed7f145d3463818fdb
Staijn1 <stein@jnkr.eu>
2024-01-16 21:18:01 +0100

    Add a button to add a track to the preview of the synced playlist

--- apps/website/src/app/pages/sync-remixed-playlist/sync-remixed-playlist-page.component.ts
+++ apps/website/src/app/pages/sync-remixed-playlist/sync-remixed-playlist-page.component.ts
@@ -41,19 +41,48 @@ export class SyncRemixedPlaylistPageComponent {
 
   private load() {
     this.isLoading = true;
-
-
     this.apiService.comparePlaylists(this.leftPlaylistId, this.rightPlaylistId)
       .then(changes => {
-        // Sort all changes so added tracks are at the top, then deleted and unchanged tracks
-        changes.sort((a, b) => {
-          if (a[0] === b[0]) return 0; // If both have the same status, no need to change order
-          if (a[0] !== 0) return -1; // Changed tracks go to the top
-          if (b[0] != 0) return 1; // Changed tracks go to the top
-          return 0; // Unchanged tracks go to the bottom
-        });
-        this.missingSongsInOriginal = changes.filter(change => change[0] === -1);
-        this.draftSyncedPlaylist = changes.filter(change => change[0] !== -1);
+        const sortedChanges = this.sortDiffs(changes);
+        this.missingSongsInOriginal = sortedChanges.filter(change => change[0] === -1);
+        this.draftSyncedPlaylist = sortedChanges.filter(change => change[0] !== -1);
       }).finally(() => this.isLoading = false);
+  }
+
+  /**
+   * Moves a missing song from the original playlist to the synced playlist draft
+   * @param diff
+   */
+  addTrackToPreviewSyncedPlaylist(diff: Diff) {
+    this.draftSyncedPlaylist.push(diff);
+    this.draftSyncedPlaylist = this.sortDiffs(this.draftSyncedPlaylist);
+    this.missingSongsInOriginal = this.missingSongsInOriginal.filter(missingSong => missingSong[1].track.id !== diff[1].track.id);
+
+  }
+
+  /**
+   * Moves a song from the synced playlist draft to the missing songs in the original playlist
+   * @param diff
+   */
+  removeTrackFromPreviewSyncedPlaylist(diff: Diff) {
+    this.missingSongsInOriginal.push(diff);
+    this.missingSongsInOriginal = this.sortDiffs(this.draftSyncedPlaylist);
+    this.draftSyncedPlaylist = this.draftSyncedPlaylist.filter(missingSong => missingSong[1].track.id !== diff[1].track.id);
+  }
+
+  /**
+   * Sorts the diffs so added tracks are at the top, then deleted and then the remaining unchanged tracks
+   * @param diffs
+   * @private
+   */
+  private sortDiffs(diffs: Diff[]): Diff[] {
+    const copy = [...diffs];
+    copy.sort((a, b) => {
+      if (a[0] === b[0]) return 0; // If both have the same status, no need to change order
+      if (a[0] !== 0) return -1; // Changed tracks go to the top
+      if (b[0] != 0) return 1; // Changed tracks go to the top
+      return 0; // Unchanged tracks go to the bottom
+    });
+    return copy;
   }
 }
