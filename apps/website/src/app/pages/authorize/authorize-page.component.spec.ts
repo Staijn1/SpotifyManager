e5be74de3f3df4173d1ce497eb3484c1a101fb85
Staijn1 <stein@jnkr.eu>
2024-01-19 20:34:51 +0100

    Create authorize-page.component.spec.ts

---
+++ apps/website/src/app/pages/authorize/authorize-page.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthorizePageComponent } from './authorize-page.component';
+import { SpotifyAuthenticationService } from '../../services/spotify-authentication/spotify-authentication.service';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('AuthorizePageComponent', () => {
+  let component: AuthorizePageComponent;
+  let fixture: ComponentFixture<AuthorizePageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: SpotifyAuthenticationService,
+          useValue: {
+            completeLogin: jest.fn().mockReturnValue(Promise.resolve(true))
+          }
+        }
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AuthorizePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
