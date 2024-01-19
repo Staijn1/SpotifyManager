89c18e6cd41f951191063ebe1e458d9cf993c3be
Staijn1 <stein@jnkr.eu>
2024-01-19 20:31:03 +0100

    Make test pass

--- apps/website/src/app/pages/get-started/get-started-page.component.spec.ts
+++ apps/website/src/app/pages/get-started/get-started-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GetStartedPageComponent } from './get-started-page.component';
+import { SpotifyAuthenticationService } from '../../services/spotify-authentication/spotify-authentication.service';
 
 describe('GetStartedPageComponent', () => {
   let component: GetStartedPageComponent;
@@ -8,7 +9,9 @@ describe('GetStartedPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [GetStartedPageComponent],
-      providers: [],
+      providers: [
+        { provide: SpotifyAuthenticationService, useValue: {} }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(GetStartedPageComponent);
