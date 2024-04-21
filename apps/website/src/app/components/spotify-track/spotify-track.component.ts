99592d65c878ef8423150802431fb09342a3d56c
Staijn1 <stein@jnkr.eu>
2024-04-21 20:00:51 +0200

    Fix errors when playlist has an episode in it

--- apps/website/src/app/components/spotify-track/spotify-track.component.ts
+++ apps/website/src/app/components/spotify-track/spotify-track.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { ArtistObjectSimplified, EpisodeObjectFull, PlaylistTrackObject, TrackObjectFull } from '@spotify-manager/core';
+import {
+  ArtistObjectSimplified,
+  EpisodeObjectFull,
+  EpisodeObjectSimplified,
+  PlaylistTrackObject,
+  TrackObjectFull, TrackObjectSimplified
+} from '@spotify-manager/core';
 import { NgClass } from '@angular/common';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { SpotifyPreviewComponent } from '../spotify-preview/spotify-preview.component';
@@ -17,37 +23,35 @@ import { SpotifyPreviewComponent } from '../spotify-preview/spotify-preview.comp
   encapsulation: ViewEncapsulation.None
 })
 export class SpotifyTrackComponent {
-  @Input({
-    transform: (value: PlaylistTrackObject | TrackObjectFull | undefined) => {
-      if (!value) return value;
-
-      // False positive
-      // eslint-disable-next-line
-      if ('track' in value) {
-        return value.track;
-      }
-      return value;
-    }
-  }) track: TrackObjectFull | EpisodeObjectFull | undefined;
+  @Input() track: TrackObjectFull | EpisodeObjectFull | undefined;
   @Input() isHorizontalLayout = false;
   @Input() ranking: number | undefined;
   @Input() imageClasses = 'w-full';
   @Input() reverse = false;
   @Input() showPreview = false;
-  get previewUrl(): string | undefined {
-    if (this.track?.type == 'track') {
-      return this.track.preview_url;
-    }
 
-    return undefined;
+  get previewUrl(): string | undefined {
+    switch (this.track?.type) {
+      case 'track':
+        return this.track.preview_url;
+      case 'episode':
+        return this.track.audio_preview_url ?? undefined;
+      default:
+        return undefined;
+    }
   }
 
   get coverImage(): string {
-    if (this.track?.type == 'track') {
-      return this.track.album.images[1].url;
+    console.log(this.track);
+    if (!this.track) return '';
+    switch (this.track.type) {
+      case 'track':
+        return this.track.album.images[1].url;
+      case 'episode':
+        return this.track.album.images[0].url ?? '';
+      default:
+        return '';
     }
-
-    return this.track?.show.images[0].url ?? '';
   }
 
   get artists(): string {
