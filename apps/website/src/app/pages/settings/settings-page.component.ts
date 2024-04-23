dcaf9abbd95d9f744106ffe7834466dac1f1cff8
Stein Jonker <stein@jnkr.eu>
2024-04-23 21:15:42 +0200

    Setup for form

--- apps/website/src/app/pages/settings/settings-page.component.ts
+++ apps/website/src/app/pages/settings/settings-page.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.scss',
 })
-export class SettingsPageComponent {}
+export class SettingsPageComponent {
+  saveEmailPreferences() {
+
+  }
+}
