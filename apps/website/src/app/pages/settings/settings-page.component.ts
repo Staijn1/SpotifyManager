d35db4a268560c2f4bc087c44f3a8efed6dfca04
Staijn1 <stein@jnkr.eu>
2024-04-24 22:31:57 +0200

    Styling and introducing some functionality to page

--- apps/website/src/app/pages/settings/settings-page.component.ts
+++ apps/website/src/app/pages/settings/settings-page.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { UserPreferenceService } from '../../services/user-preference/user-preference.service';
+import { EmailNotificationFrequency, IUserPreferences, IUserPreferencesResponse } from '@spotify-manager/core';
+import { Store } from '@ngrx/store';
+import { SpotifyManagerUserState } from '../../types/SpotifyManagerUserState';
+import { ReceiveUserPreferences } from '../../redux/user-state/user-state.action';
 
 @Component({
   selector: 'app-settings-page',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './settings-page.component.html',
-  styleUrl: './settings-page.component.scss',
+  styleUrl: './settings-page.component.scss'
 })
-export class SettingsPageComponent {
-  saveEmailPreferences() {
+export class SettingsPageComponent implements OnInit {
+  availableEmailFrequencyOptions: EmailNotificationFrequency[] = [];
 
+  userPreferences: IUserPreferences = {
+    originalPlaylistChangeNotificationFrequency: EmailNotificationFrequency.WEEKLY
+  };
+
+  constructor(
+    private readonly userPreferenceService: UserPreferenceService,
+    private readonly store: Store<{ userState: SpotifyManagerUserState }>) {
+
+  }
+
+  ngOnInit() {
+    this.userPreferenceService.getEmailFrequencyOptions().then(options => this.availableEmailFrequencyOptions = options);
+  }
+
+  saveEmailPreferences() {
+    this.userPreferenceService.saveUserPreference(this.userPreferences)
+      .then((response: IUserPreferencesResponse) => {
+      this.store.dispatch(new ReceiveUserPreferences(response));
+    });
   }
 }
---
+++ apps/website/src/app/services/user-preference/user-preference.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HTTPService } from '../http/http-service.service';
+import { EmailNotificationFrequency, IUserPreferencesRequest, IUserPreferencesResponse } from '@spotify-manager/core';
+import { SpotifyAuthenticationService } from '../spotify-authentication/spotify-authentication.service';
+import { MessageService } from '../message/message.service';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserPreferenceService extends HTTPService {
+  constructor(messageService: MessageService, protected readonly spotifyAuth: SpotifyAuthenticationService) {
+    super(messageService);
+  }
+
+  async saveUserPreference(userPreferencesRequest: IUserPreferencesRequest): Promise<IUserPreferencesResponse> {
+    return this.request(`${environment.apiURL}/user-preferences`, {
+      method: 'PUT',
+      body: JSON.stringify(userPreferencesRequest),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+
+  async getEmailFrequencyOptions(): Promise<EmailNotificationFrequency[]> {
+    return this.request(`${environment.apiURL}/user-preferences/email-frequencies`, { method: 'GET' });
+  }
+}
