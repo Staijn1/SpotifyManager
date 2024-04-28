81161b65bb6c46023d98150c026addc47cdfcc1f
Staijn1 <stein@jnkr.eu>
2024-04-28 17:17:09 +0200

    Progress in fixing some tests but not all of them

--- apps/api/src/app/modules/user-preferences/controllers/user-preferences.controller.spec.ts
+++ apps/api/src/app/modules/user-preferences/controllers/user-preferences.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserPreferencesController } from './user-preferences.controller';
+import { UserPreferencesService } from '../services/user-preferences.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { repositoryMockFactory } from '../../../utilities/testing-utils';
+import { UserPreferencesEntity } from '../entities/user-preferences.entity';
 
 describe('UserPreferencesController', () => {
   let controller: UserPreferencesController;
@@ -7,6 +11,13 @@ describe('UserPreferencesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserPreferencesController],
+      providers: [
+        { provide: UserPreferencesService, useValue: jest.fn() },
+        {
+          provide: getRepositoryToken(UserPreferencesEntity),
+          useValue: repositoryMockFactory
+        }
+      ]
     }).compile();
 
     controller = module.get<UserPreferencesController>(
--- apps/website/src/app/pages/settings/settings-page.component.spec.ts
+++ apps/website/src/app/pages/settings/settings-page.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SettingsPageComponent } from './settings-page.component';
+import { provideMockStore } from '@ngrx/store/testing';
+import { UserPreferenceService } from '../../services/user-preference/user-preference.service';
 
 describe('SettingsPageComponent', () => {
   let component: SettingsPageComponent;
@@ -8,6 +10,17 @@ describe('SettingsPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SettingsPageComponent],
+      providers: [
+        {
+          provide: UserPreferenceService,
+          useValue: {
+            getUserPreferences: jest.fn().mockResolvedValue({}),
+            saveUserPreference: jest.fn().mockResolvedValue({}),
+            getEmailFrequencyOptions: jest.fn().mockResolvedValue([])
+          },
+        },
+        provideMockStore({})
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SettingsPageComponent);
