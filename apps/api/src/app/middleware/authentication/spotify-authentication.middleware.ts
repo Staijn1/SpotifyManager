36f6bff227a2759df9c94e9afb84f11efe625bb5
Staijn1 <stein@jnkr.eu>
2024-04-28 21:58:04 +0200

    Improve logging message

--- apps/api/src/app/middleware/authentication/spotify-authentication.middleware.ts
+++ apps/api/src/app/middleware/authentication/spotify-authentication.middleware.ts
@@ -20,7 +20,7 @@ export class SpotifyAuthenticationMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     // The access token can be sent as a query parameter, in the body or as a Bearer token in the Authorization header.
     let accessToken = req.query.accessToken || req.body.accessToken || req.headers.authorization;
-    if (!accessToken) throw new HttpException('Access token not provided', HttpStatus.UNAUTHORIZED);
+    if (!accessToken) throw new HttpException('Spotify access token not provided', HttpStatus.UNAUTHORIZED);
 
     accessToken = accessToken.replace('Bearer ', '');
     // todo error handling, invalid token crashes the api... wtf?
