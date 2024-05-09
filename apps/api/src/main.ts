4864ce97e63b774ec70a7cf14091de88969523f0
Staijn1 <stein@jnkr.eu>
2024-05-09 09:50:37 +0200

    Setup logging with seq

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,9 @@
 
 import { INestApplication, Logger } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-
+import { WinstonModule } from 'nest-winston';
+import { SeqTransport } from '@datalust/winston-seq';
+import * as winston from 'winston';
 import { AppModule } from './app/app.module';
 import { json, urlencoded } from 'express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -24,10 +26,52 @@ function setupSwagger(app: INestApplication, swaggerUrl: string) {
   SwaggerModule.setup(swaggerUrl, app, document);
 }
 
+/**
+ * Create a logger for the application using Winston instead of the built-in nestjs logger.
+ * Allows for logging to multiple transports, such as the console and Seq, or modifying the log format.
+ */
+const createLogger = () => {
+  const logLevel = process.env.MIN_LOG_LEVEL ?? 'info';
+  return WinstonModule.createLogger({
+    level: logLevel,
+    format: winston.format.combine(   /* This is required to get errors to log with stack traces. See https://github.com/winstonjs/winston/issues/1498 */
+      winston.format.errors({stack: true}),
+      winston.format.json(),
+    ),
+    defaultMeta: {
+      Application: 'SpotifyManager',
+      Environment: process.env.NODE_ENV || 'Local',
+    },
+    transports: [
+      // log everything to the console
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.colorize({
+            all: true,
+          }),
+          winston.format.simple(),
+        ),
+      }),
+      new SeqTransport({
+        serverUrl: process.env.SEQ_URL ?? 'http://localhost:5341',
+        apiKey: undefined,
+        onError: (e => {
+          console.error(e);
+        }),
+        handleExceptions: true,
+        handleRejections: true,
+      }),
+    ],
+  });
+};
+
 async function bootstrap() {
   const globalPrefix = 'api';
-  const port = process.env.PORT || 3333;
-  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3333;
+  const app = await NestFactory.create(AppModule, {
+    logger: createLogger(),
+  });
+
   const adapterHost = app.get(HttpAdapterHost);
   app.setGlobalPrefix(globalPrefix);
   setupSwagger(app, globalPrefix);
