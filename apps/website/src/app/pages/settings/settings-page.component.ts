6b0cf4c653eff5b85a1ab13778f87e7485acb362
Staijn1 <stein@jnkr.eu>
2024-05-26 17:16:08 +0200

    Build user interface

--- apps/website/src/app/pages/settings/settings-page.component.ts
+++ apps/website/src/app/pages/settings/settings-page.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UserPreferenceService } from '../../services/user-preference/user-preference.service';
-import { EmailNotificationFrequency, IUserPreferences, IUserPreferencesResponse } from '@spotify-manager/core';
+import {
+  EmailNotificationFrequency,
+  IUserPreferences,
+  IUserPreferencesResponse,
+  ListOfUsersPlaylistsResponse
+} from '@spotify-manager/core';
 import { Store } from '@ngrx/store';
 import { SpotifyManagerUserState } from '../../types/SpotifyManagerUserState';
 import { ReceiveUserPreferences } from '../../redux/user-state/user-state.action';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { map } from 'rxjs';
+import { ApiService } from '../../services/api/api.service';
 
 @Component({
   selector: 'app-settings-page',
   standalone: true,
-  imports: [CommonModule, FormsModule, FaIconComponent],
+  imports: [CommonModule, FormsModule, FaIconComponent, NgOptimizedImage],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.scss'
 })
 export class SettingsPageComponent implements OnInit {
   readonly informationIcon = faInfoCircle;
+  remixes: SpotifyApi.ListOfUsersPlaylistsResponse | undefined;
   availableEmailFrequencyOptions: EmailNotificationFrequency[] = [];
 
   // Defaults for when the user first logs in and has no preferences
@@ -28,9 +35,11 @@ export class SettingsPageComponent implements OnInit {
   };
   hasUserPreferencesSet = false;
   protected isInitializing = true;
+  private isLoading = true;
 
   constructor(
     private readonly userPreferenceService: UserPreferenceService,
+    private readonly api: ApiService,
     private readonly store: Store<{ userState: SpotifyManagerUserState }>) {
     this.store.select('userState')
       .pipe(map(state => state.userPreferences))
@@ -47,6 +56,18 @@ export class SettingsPageComponent implements OnInit {
     this.userPreferenceService.getEmailFrequencyOptions()
       .then(options => this.availableEmailFrequencyOptions = options)
       .finally(() => this.isInitializing = false)
+
+    this.getRemixedPlaylists();
+  }
+
+  /**
+   * Get the remixed playlists for this user
+   */
+  getRemixedPlaylists(): void {
+    this.isLoading = true;
+    this.api.getMyRemixedPlaylists()
+      .then(data => this.remixes = data as ListOfUsersPlaylistsResponse)
+      .finally(() => this.isLoading = false);
   }
 
   saveEmailPreferences() {
