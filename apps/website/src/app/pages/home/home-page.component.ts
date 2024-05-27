09e79f6db323057883bbf8c41e621fc0f0669041
Staijn1 <stein@jnkr.eu>
2024-05-27 21:26:49 +0200

    Start with new style

--- apps/website/src/app/pages/home/home-page.component.ts
+++ apps/website/src/app/pages/home/home-page.component.ts
@@ -3,14 +3,30 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { cssDisc, cssMail } from '@ng-icons/css.gg';
+
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, RouterLink, FaIconComponent],
+  imports: [CommonModule, RouterLink, FaIconComponent, NgIcon],
+  providers: [provideIcons({ cssDisc, cssMail })],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss',
+  styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
   rightArrowIcon = faArrowRight;
+  readonly features = [
+    {
+      icon: 'cssDisc',
+      title: 'Playlist Remixing',
+      description: "Want more control over playlists that are not yours?<br>With Playlist Remixing, you can add or remove any song from any playlist and synchronize your version with the original"
+    },
+    {
+      icon: 'cssMail',
+      title: "Email Notifications",
+      description: "Get notified when the original playlist of your remix has been updated."
+    }
+  ];
 }
--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -10,5 +10,34 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: '#167bff',
+          'primary-content': '#ffffff',
+          'secondary': '#494949',
+          'secondary-content': '#d8d8d8',
+          'accent': '#00c7b5',
+          'accent-content': '#000e0c',
+          'neutral': '#2a323c',
+          'neutral-content': '#a6adbb',
+          'base-100': '#191e23',
+          'base-200': '#252b32',
+          'base-300': '#2a3038',
+          'base-content': '#dcebfa',
+          'info': '#00e1ff',
+          'info-content': '#001216',
+          'success': '#24b75e',
+          'success-content': '#ffffff',
+          'warning': '#f5a524',
+          'warning-content': '#150a00',
+          'error': '#dd204b',
+          'error-content': '#ffffff',
+        }
+      }
+    ],
+  },
   plugins: [require("daisyui")],
 };
