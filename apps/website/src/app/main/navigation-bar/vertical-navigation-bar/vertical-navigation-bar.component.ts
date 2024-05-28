1b34ed4a91030d37519963201be2423ec4ef5bf9
Staijn1 <stein@jnkr.eu>
2024-05-28 21:50:12 +0200

    Cleanup of new navigation bar

--- apps/website/src/app/main/navigation-bar/vertical-navigation-bar/vertical-navigation-bar.component.ts
+++ apps/website/src/app/main/navigation-bar/vertical-navigation-bar/vertical-navigation-bar.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { cssDisc, cssHome } from '@ng-icons/css.gg';
+import { heroBookOpen } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-vertical-navigation-bar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgIcon],
+  providers: [provideIcons({cssDisc, cssHome, heroBookOpen})],
   templateUrl: './vertical-navigation-bar.component.html',
   styleUrl: './vertical-navigation-bar.component.scss',
 })
