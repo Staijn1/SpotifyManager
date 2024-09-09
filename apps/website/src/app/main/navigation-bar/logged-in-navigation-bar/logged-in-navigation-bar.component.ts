98cdba79a50efffc5d660af3ba69cc4a696680ae
Staijn1 <stein@jnkr.eu>
2024-09-09 21:55:32 +0200

    Add cssheadset icon back in

--- apps/website/src/app/main/navigation-bar/logged-in-navigation-bar/logged-in-navigation-bar.component.ts
+++ apps/website/src/app/main/navigation-bar/logged-in-navigation-bar/logged-in-navigation-bar.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { cssDisc, cssHome } from '@ng-icons/css.gg';
+import {cssDisc, cssHeadset, cssHome} from '@ng-icons/css.gg';
 import { heroBookOpen } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-vertical-navigation-bar',
   standalone: true,
   imports: [CommonModule, RouterLink, NgIcon, RouterLinkActive],
-  providers: [provideIcons({cssDisc, cssHome, heroBookOpen})],
+  providers: [provideIcons({cssDisc, cssHome, heroBookOpen, cssHeadset})],
   templateUrl: './logged-in-navigation-bar.component.html',
   styleUrl: './logged-in-navigation-bar.component.scss',
 })
