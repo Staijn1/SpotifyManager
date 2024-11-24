22d55fb3ceddd1bba22063a14348f384b715ed92
Staijn1 <stein@jnkr.eu>
2024-11-24 20:33:47 +0100

    WIP: Show Arrow Up and Arrow Down to visualize changes in playlist

--- apps/website/src/app/pages/apps/dj-mode-details/dj-mode-details-page.component.ts
+++ apps/website/src/app/pages/apps/dj-mode-details/dj-mode-details-page.component.ts
@@ -1,8 +1,8 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {AudioFeaturesObject, ListOfUsersPlaylistsResponse, TrackObjectFull} from "@spotify-manager/core";
-import {ApiService} from "../../../services/api/api.service";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AudioFeaturesObject, SinglePlaylistResponse, TrackObjectFull } from '@spotify-manager/core';
+import { ApiService } from '../../../services/api/api.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,29 +10,13 @@ import { ActivatedRoute } from '@angular/router';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './dj-mode-details-page.component.html',
-  styleUrl: './dj-mode-details-page.component.scss',
+  styleUrls: ['./dj-mode-details-page.component.scss']
 })
-export class DjModeDetailsPageComponent implements OnInit, AfterViewInit {
+export class DjModeDetailsPageComponent implements OnInit {
   playlistId = '';
-  userplaylists: ListOfUsersPlaylistsResponse | undefined;
-  private sortedPlaylist: { track: TrackObjectFull, audioFeatures: AudioFeaturesObject, score: number }[] = [];
-
-  originalOrder = [
-    { id: 1, name: 'Song A' },
-    { id: 2, name: 'Song B' },
-    { id: 3, name: 'Song C' }
-  ];
-
-  sortedOrder = [
-    { id: 2, name: 'Song B' },
-    { id: 1, name: 'Song A' },
-    { id: 3, name: 'Song C' }
-  ];
-
-  svgLines: Array<{ x1: number; y1: number; x2: number; y2: number }> = [];
-  svgWidth = 500; // Adjust as necessary
-  svgHeight = 300; // Adjust as necessary
-
+  protected sortedPlaylist: { track: TrackObjectFull, audioFeatures: AudioFeaturesObject, score: number }[] = [];
+  protected songMovements: { positionsMoved: number; name: string; id: string; direction: 'none' | 'up' | 'down' }[] = [];
+  protected currentPlaylist: SinglePlaylistResponse | undefined;
 
   constructor(
     private readonly apiService: ApiService,
@@ -42,30 +26,22 @@ export class DjModeDetailsPageComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       this.playlistId = params.get('playlistId') || '';
-      this.getPlaylists();
-    });
-  }
 
-  ngAfterViewInit() {
-    this.calculateLines();
-  }
+      const promises = [
+        this.apiService.getPlaylist(this.playlistId),
+        this.apiService.djModePlaylist(this.playlistId)
+      ];
 
-  getPlaylists(): void {
-    this.apiService.getAllUserPlaylists().then(userplaylists => {
-      userplaylists.items.sort((a, b) => a.name.localeCompare(b.name));
-      this.userplaylists = userplaylists;
-    });
-  }
+      Promise.all(promises).then(([playlist, sortedPlaylist]) => {
+        this.currentPlaylist = playlist as SinglePlaylistResponse;
+        this.sortedPlaylist = sortedPlaylist as {
+          track: TrackObjectFull,
+          audioFeatures: AudioFeaturesObject,
+          score: number
+        }[];
 
-  getSuggestedSorting(): void {
-    this.apiService.djModePlaylist(this.playlistId).then(sortedPlaylist => {
-      this.sortedPlaylist = sortedPlaylist as { track: TrackObjectFull, audioFeatures: AudioFeaturesObject, score: number }[];
-      const nameUriPairs = this.sortedPlaylist.map(x => ({name: x.track.name, uri: x.track.uri, score: x.score}));
-      console.log(nameUriPairs);
-
-      // log any duplicates names
-      const duplicates = nameUriPairs.filter((x, i) => nameUriPairs.slice(i + 1).some(y => y.name === x.name));
-      console.log('Duplicates:', duplicates);
+        this.calculateMovements();
+      });
     });
   }
 
@@ -75,31 +51,28 @@ export class DjModeDetailsPageComponent implements OnInit, AfterViewInit {
     });
   }
 
-  calculateLines() {
-    const originalList = document.querySelectorAll('[data-index][data-order="original"]');
-    const sortedList = document.querySelectorAll('[data-index][data-order="sorted"]');
-
-    if (originalList.length === 0 || sortedList.length === 0) {
-      console.error('Elements not found properly');
+  calculateMovements() {
+    if (!this.currentPlaylist || !this.sortedPlaylist) {
       return;
     }
 
-    this.svgLines = this.originalOrder.map((song, index) => {
-      const originalElement = originalList[index] as HTMLElement;
-      const sortedIndex = this.sortedOrder.findIndex((s) => s.id === song.id);
-      const sortedElement = sortedList[sortedIndex] as HTMLElement;
+    this.songMovements = this.currentPlaylist.tracks.items.map((song, index) => {
+      const sortedIndex = this.sortedPlaylist.findIndex((s) => s.track.id === song.track.id);
+      const positionsMoved = index - sortedIndex;
+      let direction: 'up' | 'down' | 'none' = 'none';
 
-      if (originalElement && sortedElement) {
-        return {
-          x1: 0, // Adjust based on column positioning
-          y1: originalElement.offsetTop + 20, // Adjust to center vertically within 40px height
-          x2: this.svgWidth, // Adjust based on column positioning
-          y2: sortedElement.offsetTop + 20 // Adjust to center vertically within 40px height
-        };
-      } else {
-        console.warn(`Element not found for song ID: ${song.id}`);
-        return { x1: 0, y1: 0, x2: 0, y2: 0 };
+      if (positionsMoved > 0) {
+        direction = 'up';
+      } else if (positionsMoved < 0) {
+        direction = 'down';
       }
+
+      return {
+        id: song.track.id,
+        name: song.track.name,
+        direction: direction,
+        positionsMoved: Math.abs(positionsMoved)
+      };
     });
   }
 }
